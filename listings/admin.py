5904a84cf45e7dbaba4d72ba152978e3cceb17c3
Nik Pietanze <nikpietanze@gmail.com>
2019-10-21 20:52:30 -0400

    linting updated

--- listings/admin.py
+++ listings/admin.py
@@ -4,12 +4,13 @@ from .models import Listing
 
 
 class ListingAdmin(admin.ModelAdmin):
-  list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
-  list_display_links = ('title',)
-  list_filter = ('realtor',)
-  list_editable = ('is_published',)
-  search_fields = ('title', 'address', 'city', 'state', 'zipcode', 'price')
-  list_per_page = 25
+    list_display = ('id', 'title', 'is_published',
+                    'price', 'list_date', 'realtor')
+    list_display_links = ('title',)
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'address', 'city', 'state', 'zipcode', 'price')
+    list_per_page = 25
 
 
 admin.site.register(Listing, ListingAdmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -4,10 +4,10 @@ from .models import Realtor
 
 
 class RealtorAdmin(admin.ModelAdmin):
-  list_display = ('id', 'name', 'email', 'hire_date')
-  list_display_links = ('name',)
-  search_fields = ('name',)
-  list_per_page = 25
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('name',)
+    search_fields = ('name',)
+    list_per_page = 25
 
 
 admin.site.register(Realtor, RealtorAdmin)
